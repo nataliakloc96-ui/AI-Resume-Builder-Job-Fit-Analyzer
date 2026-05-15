85d4868e3a48597b52251cac83f7c10a4fa43abb
Natalia <6991nk@gmail.com>
2026-05-15 15:20:30 +0200

    Add notifications

--- backend/notifications.py
+++ backend/notifications.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+TOKEN = "8958954903:AAHY5iUd1XF9-JCQbUPyvuTe4k-3_FxjPiA"
+CHAT_ID = "8163340105"
 
 def send_alert(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
