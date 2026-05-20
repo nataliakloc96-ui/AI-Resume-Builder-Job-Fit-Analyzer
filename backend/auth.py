8c670b4ec018c8aea9c3f06a7001f14077a9085d
Natalia <6991nk@gmail.com>
2026-05-21 00:14:42 +0200

    Force bcrypt truncate fix

--- backend/auth.py
+++ backend/auth.py
@@ -1,22 +1,25 @@
-from jose import jwt 
+from jose import jwt
 from passlib.context import CryptContext
-import os 
+import os
 from datetime import datetime, timedelta
 
 SECRET = os.getenv("JWT_SECRET", "supersecret")
 
-pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd = CryptContext(schemes=["bcrypt"])
+
 
 def hash_password(password):
     return pwd.hash(password[:72])
 
+
 def verify_password(password, hashed):
     return pwd.verify(password[:72], hashed)
+
 
 def create_token(email):
     payload = {
         "sub": email,
         "exp": datetime.utcnow() + timedelta(days=7)
-
     }
+
     return jwt.encode(payload, SECRET, algorithm="HS256")
